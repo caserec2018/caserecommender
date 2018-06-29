ac8ae43e39055319a1071e69a3b8600279e62db5
caserec2018 <40644861+caserec2018@users.noreply.github.com>
2018-06-29 09:33:46 -0300

    remove user for review

--- examples/ranking_knn.py
+++ examples/ranking_knn.py
@@ -12,16 +12,16 @@ from caserec.recommenders.item_recommendation.itemknn import ItemKNN
 from caserec.recommenders.item_recommendation.userknn import UserKNN
 from caserec.utils.cross_validation import CrossValidation
 
-db = '/home/fortesarthur/Documentos/dataset/ml-100k/u.data'
-folds_path = '/home/fortesarthur/Documentos/dataset/ml-100k/'
+db = '/home/user/Documentos/dataset/ml-100k/u.data'
+folds_path = '/home/user/Documentos/dataset/ml-100k/'
 
-metadata_item = '/home/fortesarthur/Documentos/dataset/ml-100k/db_item_subject.dat'
-sm_item = '/home/fortesarthur/Documentos/dataset/ml-100k/sim_item.dat'
-metadata_user = '/home/fortesarthur/Documentos/dataset/ml-100k/metadata_user.dat'
-sm_user = '/home/fortesarthur/Documentos/dataset/ml-100k/sim_user.dat'
+metadata_item = '/home/user/Documentos/dataset/ml-100k/db_item_subject.dat'
+sm_item = '/home/user/Documentos/dataset/ml-100k/sim_item.dat'
+metadata_user = '/home/user/Documentos/dataset/ml-100k/metadata_user.dat'
+sm_user = '/home/user/Documentos/dataset/ml-100k/sim_user.dat'
 
-tr = '/home/fortesarthur/Documentos/dataset/ml-100k/folds/0/train.dat'
-te = '/home/fortesarthur/Documentos/dataset/ml-100k/folds/0/test.dat'
+tr = '/home/user/Documentos/dataset/ml-100k/folds/0/train.dat'
+te = '/home/user/Documentos/dataset/ml-100k/folds/0/test.dat'
 
 """
 
