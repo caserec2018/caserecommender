9467447df22aabc952fe1b1476f90b8661ab8291
caserec2018 <40644861+caserec2018@users.noreply.github.com>
2018-06-29 09:34:42 -0300

    remove user for review

--- examples/ranking_others.py
+++ examples/ranking_others.py
@@ -5,8 +5,8 @@
 """
 from caserec.recommenders.item_recommendation.bprmf import BprMF
 
-tr = '/home/fortesarthur/Documentos/dataset/ml-100k/folds/0/train.dat'
-te = '/home/fortesarthur/Documentos/dataset/ml-100k/folds/0/test.dat'
+tr = '/home/user/Documentos/dataset/ml-100k/folds/0/train.dat'
+te = '/home/user/Documentos/dataset/ml-100k/folds/0/test.dat'
 
 
 BprMF(tr, te, batch_size=30).compute()
