50b3e7b45c035a344701fbc40702af74fa232d25
caserec2018 <40644861+caserec2018@users.noreply.github.com>
2018-06-29 09:35:52 -0300

    remove user for review

--- examples/rating_prediction_mf.py
+++ examples/rating_prediction_mf.py
@@ -10,16 +10,16 @@ from caserec.recommenders.rating_prediction.svdplusplus import SVDPlusPlus
 from caserec.recommenders.rating_prediction.matrixfactorization import MatrixFactorization
 from caserec.utils.cross_validation import CrossValidation
 
-db = 'C:/Users/forte/OneDrive/ml-100k/u.data'
-folds_path = 'C:/Users/forte/OneDrive/ml-100k/'
+db = 'C:/Users/user/OneDrive/ml-100k/u.data'
+folds_path = 'C:/Users/user/OneDrive/ml-100k/'
 
-metadata_item = 'C:/Users/forte/OneDrive/ml-100k/db_item_subject.dat'
-sm_item = 'C:/Users/forte/OneDrive/ml-100k/sim_item.dat'
-metadata_user = 'C:/Users/forte/OneDrive/ml-100k/metadata_user.dat'
-sm_user = 'C:/Users/forte/OneDrive/ml-100k/sim_user.dat'
+metadata_item = 'C:/Users/user/OneDrive/ml-100k/db_item_subject.dat'
+sm_item = 'C:/Users/user/OneDrive/ml-100k/sim_item.dat'
+metadata_user = 'C:/Users/user/OneDrive/ml-100k/metadata_user.dat'
+sm_user = 'C:/Users/user/OneDrive/ml-100k/sim_user.dat'
 
-tr = 'C:/Users/forte/OneDrive/ml-100k/folds/0/train.dat'
-te = 'C:/Users/forte/OneDrive/ml-100k/folds/0/test.dat'
+tr = 'C:/Users/user/OneDrive/ml-100k/folds/0/train.dat'
+te = 'C:/Users/user/OneDrive/ml-100k/folds/0/test.dat'
 
 """
 
