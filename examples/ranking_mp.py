c82e385e1193a968866e37ac7683202f2ef7ddbe
caserec2018 <40644861+caserec2018@users.noreply.github.com>
2018-06-29 09:34:16 -0300

    remove user for review

--- examples/ranking_mp.py
+++ examples/ranking_mp.py
@@ -9,11 +9,11 @@
 from caserec.recommenders.item_recommendation.most_popular import MostPopular
 from caserec.utils.cross_validation import CrossValidation
 
-db = '/home/fortesarthur/Documentos/dataset/hetrec2011-movielens-2k-v2/user_ratedmovies.dat'
-folds_path = '/home/fortesarthur/Documentos/dataset/hetrec2011-movielens-2k-v2/'
+db = '/home/user/Documentos/dataset/hetrec2011-movielens-2k-v2/user_ratedmovies.dat'
+folds_path = '/home/user/Documentos/dataset/hetrec2011-movielens-2k-v2/'
 
-tr = '/home/fortesarthur/Documentos/dataset/hetrec2011-movielens-2k-v2/folds/0/train.dat'
-te = '/home/fortesarthur/Documentos/dataset/hetrec2011-movielens-2k-v2/folds/0/test.dat'
+tr = '/home/user/Documentos/dataset/hetrec2011-movielens-2k-v2/folds/0/train.dat'
+te = '/home/user/Documentos/dataset/hetrec2011-movielens-2k-v2/folds/0/test.dat'
 
 # Cross Validation
 recommender = MostPopular(as_binary=True)
